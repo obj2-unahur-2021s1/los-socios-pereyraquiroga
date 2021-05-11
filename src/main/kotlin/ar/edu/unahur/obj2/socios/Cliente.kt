c6f8b2aedb1b323ba11f27cfbb19523cac696483
Lau38 <lautiaguii@gmail.com>
2021-05-10 21:40:01 -0300

    terminar

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,54 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+  abstract class Cliente(val plataBolsillo: Int) {
+
+      abstract fun propinaSegunAnimo():Int
+ }
+
+   class EstadoAnimoFeliz(val costoPedido : Int, plataBolsillo: Int) : Cliente( plataBolsillo){
+
+    override fun propinaSegunAnimo()= costoPedido*25 /100
 }
+  class EstadoAnimoEnojado(val costoPedido: Int,  plataBolsillo: Int):Cliente(plataBolsillo){
+    override fun propinaSegunAnimo(): Int = 0
+}
+
+ class EstadoAnimoIndiferente(val costoPedido: Int,  plataBolsillo: Int):Cliente(plataBolsillo){
+    override fun propinaSegunAnimo(): Int = plataBolsillo
+}
+
+ class EstadoAnimoResfriado(val costoPedido: Int,  plataBolsillo: Int):Cliente(plataBolsillo){
+    override fun propinaSegunAnimo(): Int = costoPedido
+}
+
+interface Barrio {
+    fun propinaOtorga() :Int
+}
+
+class LasRosas() : Barrio{
+    override fun propinaOtorga() = 50
+}
+
+class LasLauchas() :Barrio{
+    override fun propinaOtorga()= 1/2
+}
+
+class BarrioVerde():Barrio{
+    override fun propinaOtorga(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class LasTorres(): Barrio{
+    override fun propinaOtorga(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+
+
+
+
+
+
+
