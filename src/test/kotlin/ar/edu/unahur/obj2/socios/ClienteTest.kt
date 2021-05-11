38b6f4f085061bfddca972514df47cd5e096da4a
Lau38 <lautiaguii@gmail.com>
2021-05-11 10:54:13 -0300

    terminar

--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -1,8 +1,15 @@
 package ar.edu.unahur.obj2.socios
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
 
 class ClienteTest : DescribeSpec({
-  describe("Un/a cliente") {
+
+  describe("Un/a cliente feliz viviendo en las rosas") {
+    val estella = EstadoAnimoFeliz(200,100)
+    estella.propinaSegunAnimo().shouldBe(50)
+
+
   }
 })
